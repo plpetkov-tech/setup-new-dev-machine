600d630ca7350f3b100cfc32877ebf8466e57a8d
plpetkov-tech <plpetkov@tutanota.com>
2020-12-30 15:56:01 +0200

    Script for docker and docker compose updated

--- installation-of-tools.sh
+++ installation-of-tools.sh
@@ -20,6 +20,12 @@ echo "****Installing Docker Compose****";
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose;
 sudo chmod +x /usr/local/bin/docker-compose;
 
+echo "****Giving permissions to Docker to not require sudo****"
+sudo usermod -aG docker ${USER};
+
+
 echo "****Checking Docker Compose installation****";
+
 docker–compose –version;
 
+su - ${USER};
